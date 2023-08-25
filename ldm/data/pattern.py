4009b97b1559fb9430c4eb6f1221f4d066f5d8c7
nirbhay-faaya <nirbhay.sharma@faaya.io>
2023-08-25 09:47:15 +0000

    adding dataloader for refined captions dataset

--- ldm/data/pattern.py
+++ ldm/data/pattern.py
@@ -173,8 +173,159 @@ class PrintGenerationData(PatternTextToImageDataset):
             img_transforms
         )
 
+
+class UpdatedPatternTextToImageDataset():
+    def __init__(self,
+                csv_files_folder: str,
+                images_folder: str,
+                img_transforms: transforms):
+
+        """
+        csv_files_format: image_name, caption, mini_caption, timetaken
+        """
+        
+        self.csv_files_path = csv_files_folder
+        self.img_path = images_folder
+        self.img_transforms = img_transforms
+
+        self.error_threshold = 80
+
+        self.csv_file_paths_map = {}
+        self.folder_dfs(
+            self.csv_files_path,
+            ['csv'],
+            self.csv_file_paths_map
+        )
+
+        self.image_file_path_map = {}
+        for data_folders in self.img_path:
+            self.folder_dfs(
+                data_folders,
+                ['jpg', 'png', 'jpeg', 'JPG', 'PNG'],
+                self.image_file_path_map
+            )  
+
+        erroneous_captions = {}
+
+        img_caption_arr = None
+        for csv_file_name, csv_path in self.csv_file_paths_map.items():
+            csv_data = pd.read_csv(csv_path)
+            csv_data['mini-caption'] = csv_data['mini-caption'].apply(lambda x: self._preprocess(str(x)))
+            # erroneous_captions[csv_file_name] = csv_data.loc[
+            #     csv_data['mini-caption'].str.len() < self.error_threshold, 
+            #     ['image_name', 'mini-caption']].values
+
+            csv_data = csv_data.loc[
+                csv_data['mini-caption'].str.len() > self.error_threshold, 
+                ['image_name', 'mini-caption']].values
+
+            if img_caption_arr is None:
+                img_caption_arr = csv_data
+            else:
+                img_caption_arr = np.concatenate([img_caption_arr, csv_data], axis=0)
+
+        img_caption_arr = dict(img_caption_arr)
+        self.image_file_path_map = dict(filter(
+            lambda x: x[0] in img_caption_arr,
+            self.image_file_path_map.items()
+        ))
+
+        # print(len(self.image_file_path_map))
+        # print(len(img_caption_arr))
+
+        # print(erroneous_captions['data_2_tail.csv'])
+
+        # img_caption_arr = list(img_caption_arr.items())
+        # for i in range(10):
+        #     image, caption = img_caption_arr[i]
+        #     print(f'{image} => {caption}')
+
+        self.data = list(map(
+            lambda x: (img_caption_arr[x[0]], x[1]),
+            self.image_file_path_map.items()
+        ))
+
+        self.len_pre_process = len(self.data)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        text, image_path = self.data[idx]
+
+        loaded_img = get_imgs(image_path, transform=self.img_transforms)
+        loaded_img = loaded_img.permute(1,2,0)
+
+        input_data = {}
+        input_data["image"] = loaded_img
+        input_data["caption"] = text
+
+        return input_data
+
+    def folder_dfs(self, 
+        dir_path: str, 
+        allowed_ext: [str], 
+        final_paths_dict: {str, str}):
+
+        cur_dirs = os.listdir(dir_path)
+
+        for dirs in cur_dirs:
+            cur_path = os.path.join(dir_path, dirs)
+            if os.path.isdir(cur_path):
+                self.folder_dfs(
+                    cur_path,
+                    allowed_ext,
+                    final_paths_dict
+                )
+            if os.path.isfile(cur_path):
+                check_ext = self._check_ext(dirs, allowed_ext)
+                if check_ext:
+                    final_paths_dict[dirs] = final_paths_dict.get(dirs, cur_path)
+    
+    def _check_ext(self, file: str, ext: [str]):
+        extracted_ext = file.split('.')[-1]
+        if extracted_ext in ext:
+            return True
+        return False
+
+    def _preprocess(self, text):
+        text = text.lower()#converting string to lowercase
+        text = re.sub(r'\d(\.|\)|-)', ' ', text)
+        text = re.sub(r'\d+ words:?', ' ', text)
+        res1 = re.sub(r'((http|https)://|www.).+?(\s|$)',' ',text)#removing links
+        res2 = re.sub(f'[{string.punctuation}]+',' ',res1)#removing non english and special characters
+        res3 = re.sub(r'[^a-z0-9A-Z\s]+',' ',res2)#removing anyother that is not consider in above
+        res4 = re.sub(r'(\n)+',' ',res3)#removing all new line characters
+        res4 = re.sub(r'\s[a-zA-Z]\s', ' ', res4) # removing any single char
+        res = re.sub(r'\s{2,}',' ',res4)#remove all the one or more consecutive occurance of sapce
+        res = res.strip()
+        return res
+
+class PatternGenTextToImageDataset(UpdatedPatternTextToImageDataset):
+    def __init__(self, **kwargs):
+
+        imsize = kwargs['size']
+        root_path = kwargs['root_path']
+        img_transforms = transforms.Compose([
+            transforms.Resize((imsize,imsize)),
+            transforms.ToTensor()
+        ])
+        
+        csv_path = os.path.join(root_path, 'short')
+
+        image_folder_paths = [
+            os.path.join(root_path, f'dataset{i}') for i in [2,4,5,6]
+        ]
+
+        super().__init__(
+            csv_path,
+            image_folder_paths,
+            img_transforms
+        )
+
 if __name__ == "__main__":
-    pattern_data = PrintGenerationData(size=256, root_path='/root')
+    # pattern_data = PrintGenerationData(size=256, root_path='/root')
+    pattern_data = PatternGenTextToImageDataset(size=256, root_path='/workspace/dataset')
     input_data = pattern_data[0]
     print(input_data["image"].shape)
     print(input_data["caption"])
