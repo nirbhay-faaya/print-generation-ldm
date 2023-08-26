bdee52d2afc97e08372de25b3ebaba4d7c2ae919
nirbhay-faaya <nirbhay.sharma@faaya.io>
2023-08-26 12:13:51 +0000

    checking trained LDM model on custom prompt

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -104,8 +104,9 @@ if __name__ == "__main__":
     opt = parser.parse_args()
 
 
-    config = OmegaConf.load("configs/latent-diffusion/txt2img-1p4B-eval.yaml")  # TODO: Optionally download from same location as ckpt and chnage this logic
-    model = load_model_from_config(config, "models/ldm/text2img-large/model.ckpt")  # TODO: check path
+    config = OmegaConf.load("/workspace/ldm/configs/latent-diffusion/txt2img-1p4B-eval.yaml")  # TODO: Optionally download from same location as ckpt and chnage this logic
+    checkpoint_path = "/workspace/ldm/logs/2023-08-25T14-51-39_txt2img-latent-diffusion/checkpoints/last.ckpt"
+    model = load_model_from_config(config, checkpoint_path)  # TODO: check path
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model = model.to(device)
